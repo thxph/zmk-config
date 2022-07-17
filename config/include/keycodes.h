a4dd134bdeb4fb63b585ef9a8fa4f7dd994e952b
Thanh X. Pham <thanh.x.ph@gmail.com>
2022-07-18 02:25:50 +0700

    Add: homerow mods for corne

--- config/include/keycodes.h
+++ config/include/keycodes.h
@@ -10,3 +10,18 @@
 
 #define btlder &bootloader
 
+#define LGUI_G  &hm_retro_slow_l LGUI  G
+#define LSFT_F  &hm_fast_l       LSHFT F
+#define LCTL_D  &hm_fast_l       LCTRL D
+#define LALT_S  &hm_l            LALT  S
+#define L1_A    &hl_l            L1    A
+#define R1_ESC  &hl_fast_l       R1    ESCAPE
+
+#define RGUI_H  &hm_retro_slow_r RGUI  H
+#define RSFT_J  &hm_fast_r       RSHFT J
+#define RCTL_K  &hm_r            RCTRL K
+#define RALT_L  &hm_r            RALT  L
+#define L1_SEM  &hl_fast_r       L1    SEMI
+#define R1_BSPC &hl_fast_r       R1    BSPC
+
+#define L1_SPC  &ltp L1 SPACE
