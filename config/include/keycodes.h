369b94576d3c5ffdda6832f5aeef011b7a930823
Thanh X. Pham <thanh.x.ph@gmail.com>
2022-07-18 13:07:08 +0700

    Add: update keymap

--- config/include/keycodes.h
+++ config/include/keycodes.h
@@ -15,13 +15,13 @@
 #define LCTL_D  &hm_fast_l       LCTRL D
 #define LALT_S  &hm_l            LALT  S
 #define L1_A    &hl_l            L1    A
-#define R1_ESC  &hl_fast_l       R1    ESCAPE
+#define R1_ESC  &lthp            R1    ESCAPE
 
 #define RGUI_H  &hm_retro_slow_r RGUI  H
 #define RSFT_J  &hm_fast_r       RSHFT J
 #define RCTL_K  &hm_fast_r       RCTRL K
 #define RALT_L  &hm_r            RALT  L
 #define L1_SEM  &hl_fast_r       L1    SEMI
-#define R1_BSPC &hl_fast_r       R1    BSPC
+#define R1_BSPC &lthp            R1    BSPC
 
 #define L1_SPC  &ltp L1 SPACE
